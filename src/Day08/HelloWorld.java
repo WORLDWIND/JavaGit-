166c15446bd60b98cc08ceb2c833e5c06d9c2cc9
worldwind <1772830365@qq.com>
2023-02-20 15:47:13 +0800

    1

---
+++ src/Day08/HelloWorld.java
@@ -0,0 +1,12 @@
+package Day08;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/2/20  15:46
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("nice to meet you to die");
+    }
+}
