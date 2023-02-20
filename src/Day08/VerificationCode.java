e84a6df1313477514ad526e5b159211774931e58
worldwind <1772830365@qq.com>
2023-02-20 16:27:38 +0800

    1

---
+++ src/Day08/VerificationCode.java
@@ -0,0 +1,21 @@
+package Day08;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/2/20  16:10
+ **/
+public class VerificationCode {
+    public static void main(String[] args) {
+        /**
+         * 需求:生成?位验证码(大写字母,小写字母,数字)
+         */
+
+    }
+    // TODO 生成验证码
+    public static String createVerificationCode(int len){
+        String code = "";
+        String num = Math.random()*9+"";
+        return code;
+    }
+}
