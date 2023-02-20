fc3523e056e61ffe4923ba089aad11f1b74e6b2a
worldwind <1772830365@qq.com>
2023-02-20 21:52:57 +0800

    1

---
+++ src/Day08/HomeWork/JavaGitTest.java
@@ -0,0 +1,12 @@
+package Day08.HomeWork;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/2/20  21:52
+ **/
+public class JavaGitTest {
+    public static void main(String[] args) {
+        System.out.println("Git牛逼,我最强");
+    }
+}
