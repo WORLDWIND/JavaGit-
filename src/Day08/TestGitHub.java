ae29c38f89d6c028e1585fc258c58585dfbd96b1
worldwind <1772830365@qq.com>
2023-02-21 09:55:37 +0800

    1

---
+++ src/Day08/TestGitHub.java
@@ -0,0 +1,12 @@
+package Day08;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/2/21  09:55
+ **/
+public class TestGitHub {
+    public static void main(String[] args) {
+        System.out.println("Test GitHub");
+    }
+}
