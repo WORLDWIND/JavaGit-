0de64f836b1454692c41e8cff75bf78fb5f5406f
WORLDWIND <1772830365@qq.com>
2023-02-22 15:11:28 +0800

    1

--- src/OOPDay02/Persion.java
+++ src/OOPDay02/Persion.java
@@ -9,64 +9,88 @@ public class Persion {
     String name;
     int age;
     String address;
-    Persion(){
+
+    Persion() {
 
     }
-    Persion(String name, int age, String address){
+
+    Persion(String name, int age, String address) {
         this.name = name;
-        this.age= age;
+        this.age = age;
         this.address = address;
     }
-    void eat(){
+
+    void eat() {
         System.out.println(name + "正在吃饭");
     }
-    void sleep(){
+
+    void sleep() {
         System.out.println(name + "正在睡觉");
     }
-    void sayHi(){
+
+    void sayHi() {
         System.out.println("大家好,我叫:" + name + ",今年" + age + "岁了,家住:" + address);
     }
 }
-class Student extends  Persion{
+
+class Student extends Persion {
     String className;
     String stuId;
     String name;
-    Student(){
+
+    Student() {
         super();
     }
-    Student(String name,int age,String address,String className,String stuId){
-        super(name,age,address);
+
+    Student(String name, int age, String address, String className, String stuId) {
+        super(name, age, address);
         this.className = className;
         this.stuId = stuId;
     }
-    void study(){
+
+    void study() {
         System.out.println(name + "正在学习");
     }
+
+    void sayHi() {
+        System.out.println("大家好,我叫:" + name + ",今年" + age + "岁了,家住:" + address + ",我的班级名称是:" + className + "我的学号是:" + stuId);
+    }
 }
-class Teacher extends Persion{
+
+class Teacher extends Persion {
     double salary;
-    Teacher(){
+
+    Teacher() {
         super();
     }
-    Teacher(String name,int age,String address,double salary){
-        super(name,age,address);
+
+    Teacher(String name, int age, String address, double salary) {
+        super(name, age, address);
         this.salary = salary;
     }
-    void teach(){
-        System.out.println(name+"正在上课");
+
+    void teach() {
+        System.out.println(name + "正在上课");
+    }
+
+    void sayHi() {
+        System.out.println("大家好,我叫:" + name + ",今年" + age + "岁了,家住:" + address + "我的薪资是:" + salary);
     }
 }
-class Doctor extends Persion{
+
+class Doctor extends Persion {
     String title;
+
     Doctor() {
         super();
     }
 
-    Doctor(String name, int age, String address, String title){
-        super(name,age,address);
+    Doctor(String name, int age, String address, String title) {
+        super(name, age, address);
         this.title = title;
     }
-    void cut(){
+
+    void cut() {
         System.out.println(name + "正在做手术");
     }
 }
