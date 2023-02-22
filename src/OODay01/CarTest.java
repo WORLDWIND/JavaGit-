643f8f36c3a34c2288854faca89255f6e771d65a
worldwind <1772830365@qq.com>
2023-02-22 08:38:48 +0800

    1

--- src/OODay01/CarTest.java
+++ src/OODay01/CarTest.java
@@ -12,6 +12,6 @@ public class CarTest {
         car.Running();
         car.Stop();
         System.out.println("GitTest看看能不能传上去");
-        System.out.println("在实一次?");
+        System.out.println("在试一次?");
     }
 }
