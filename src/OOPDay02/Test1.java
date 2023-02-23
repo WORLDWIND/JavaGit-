83831a270eab7c9bc379c1267d76d8fa5cb8cf37
WORLDWIND <1772830365@qq.com>
2023-02-23 10:19:52 +0800

    1

---
+++ src/OOPDay02/Test1.java
@@ -0,0 +1,17 @@
+package OOPDay02;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/2/23  08:28
+ **/
+//TODO 构造方法的测试
+public class Test1 {
+    String name;
+    int age;
+    //构造方法
+    Test1(){
+        name = "Hello";
+        age = 10;
+    }
+}
---
+++ src/OOPDay02/Test1test.java
@@ -0,0 +1,14 @@
+package OOPDay02;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/2/23  08:28
+ **/
+public class Test1test {
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        System.out.println(test1.name);
+        System.out.println(test1.age);
+    }
+}
