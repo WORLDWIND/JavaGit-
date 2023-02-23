ebe6ad975f59e818c9bba0814fc30a896ae05f2d
WORLDWIND <1772830365@qq.com>
2023-02-23 16:17:08 +0800

    1

---
+++ src/OOPDay03/ArrayTest.java
@@ -0,0 +1,18 @@
+package OOPDay03;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/2/23  16:03
+ **/
+public class ArrayTest {
+    public static void main(String[] args) {
+        Dog[] dogs = new Dog[10];
+        dogs[0] = new Dog();
+        int[] i = new int[1];
+        i[0] = 10;
+    }
+}
+class Dog1{
+
+}
