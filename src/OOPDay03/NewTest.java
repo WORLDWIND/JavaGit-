d76ed723a505f084219d1f780c96e13745cb3a4d
Tedu <1772803065@qq.com>
2023-02-24 08:26:22 +0800

    补课班电脑的Git测试

---
+++ src/OOPDay03/NewTest.java
@@ -0,0 +1,13 @@
+package OOPDay03;
+
+/**
+ * @Author Mr.WorldWind
+ * @Date 2023/2/24 8:25
+ * @PackageName: OOPDay03
+ * @ClassName: NewTest
+ */
+public class NewTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
