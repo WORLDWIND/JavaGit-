b96736948fd65edabd44b9fb61a844673f6a629e
WORLDWIND <1772830365@qq.com>
2023-03-06 19:37:07 +0800

    1

--- src/OOPDay10/NightWork/Test06.java
+++ src/OOPDay10/NightWork/Test06.java
@@ -11,10 +11,11 @@ public class Test06 {
     public static void main(String[] args) {
         //TODO 运用匿名内部类和lambda表达式所写
         File dir = new File("./test1");
-        File[] files = dir.listFiles(file -> {
+       /* File[] files = dir.listFiles(file -> {
             //return file.getName().matches(".*s.*");
             return file.getName().contains("s");
-        });
+        });*/
+        File[] files = dir.listFiles((pathname) -> pathname.getName().contains("s"));
         for (File file : files) {
             System.out.println(file.getName());
         }
