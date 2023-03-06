2298f191952aeb7fc99134416ae4e2f0465b9962
WORLDWIND <1772830365@qq.com>
2023-03-06 15:58:34 +0800

    1

--- src/OOPDay10/MkDirDemo.java
+++ src/OOPDay10/MkDirDemo.java
@@ -10,11 +10,12 @@ import java.io.File;
  **/
 public class MkDirDemo {
     public static void main(String[] args) {
-        File file = new File("./demo");
+        File file = new File("./a/b/c");
         if(file.exists()){
             System.out.println("该目录已经存在");
         }else {
-            file.mkdir();//创建目录所存在的目录必须存在,若不存在不会报错,但也不会创建
+            //file.mkdir();//创建目录所存在的目录必须存在,若不存在不会报错,但也不会创建
+            file.mkdirs();//该方法创建目录的时候回连同将所有不存在的目录一起创建(推荐使用)
             System.out.println("创建成功");
         }
     }
