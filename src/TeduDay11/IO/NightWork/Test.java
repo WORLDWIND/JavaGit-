177970e1c926f6cbdac580f2b6881f2d9b6c25a1
WORLDWIND <1772830365@qq.com>
2023-03-07 20:53:37 +0800

    1

---
+++ src/TeduDay11/IO/NightWork/Test.java
@@ -0,0 +1,24 @@
+package TeduDay11.IO.NightWork;
+
+/**
+ * @program: Tedu
+ * @author: Mr.WorldWind
+ * @create: 2023/3/7  20:49
+ **/
+public class Test {
+    public static void main(String[] args) {
+        Aoo aoo = new Aoo();
+        aoo.a = 10;
+        Aoo boo = aoo;
+        boo.a = 100;
+        System.out.println(aoo.a);//100
+
+        int a = 1000;
+        int b = a;
+        b = 10000;
+        System.out.println(a);//1000
+    }
+}
+class Aoo{
+    int a;
+}
